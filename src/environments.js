e4e677d6bb3098ea9687b3da67560c050c56a772
akramthedev <162736569+akramthedev@users.noreply.github.com>
2025-06-07 15:14:13 +0100

    Add files via upload

---
+++ src/environments.js
@@ -0,0 +1,9 @@
+export const  ENVIRONMENT = {
+    // ENDPOINT : "http://localhost:3001", 
+    ENDPOINT : "http://universiapulse-001-site45.qtempurl.com", 
+    CLOUDINARY_PRESET : "noobtech", 
+    CLOUDINARY_CLOUD_NAME : "dqprleeyt",
+    // BACKEND_URL : "localhost:3001" // sans http !!! attention 
+    BACKEND_URL : "universiapulse-001-site45.qtempurl.com" // sans http !!! attention 
+}
+
